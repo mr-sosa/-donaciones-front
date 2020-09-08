abf3ccc0b41bcc589753fe662583d9925565be4d
Andmclein <53324948+Andmclein@users.noreply.github.com>
2020-09-08 03:29:25 -0500

    update
    No es mucho pero es trabajo honrado

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DonatorForm from './DonatorForm'
+
+const Form = () => {
+    return (
+        <div>
+          <DonatorForm/>  
+        </div>
+    )
+}
+
+export default Form
