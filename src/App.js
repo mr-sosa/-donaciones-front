7629681a68f2a3e2d7b731a56740062beb758876
Andmclein <53324948+Andmclein@users.noreply.github.com>
2020-09-11 00:12:15 -0500

    update test

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
-import React from 'react'
-import DonatorForm from './Components/DonatorForm'
-import AmountForm from './Components/AmountForm'
-import CardForm from './Components/CardForm'
+import React, { Component } from 'react'
+import DonatorForm from './components/DonatorForm'
+import AmountForm from './components/AmountForm'
+import CardForm from './components/CardForm'
 
-const App = () => {
-  return (
-    <div className='all'>
-      <div className='box-big-invisible'>
-        <div className='row'>
-          <div className='column'>
-            <h1 className='title'>Haz tu donación mensual </h1>
-            <AmountForm />
-          </div>
-          <div className='column'>
-            <div>
-              <h1 className='title-hide-on-big'>Ya casi...</h1>
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      form: {
+
+      },
+
+    };
+
+
+    // se inicializan las funciones
+    //this.changeHandler = this.changeHandler.bind(this);
+    //   this.isRegistering = false;
+  }
+
+
+  render() {
+    return (
+      <div className='all'>
+        <div className='box-big-invisible'>
+          <div className='row'>
+            <div className='column'>
+              <h1 className='title'>Haz tu donación mensual </h1>
+              <AmountForm />
             </div>
-            <div>
-              <DonatorForm />
+            <div className='column'>
+              <div>
+                <h1 className='title-hide-on-big'>Ya casi...</h1>
+              </div>
+              <div>
+                <DonatorForm />
+              </div>
             </div>
           </div>
         </div>
+        <div className='row'>
+          <CardForm />
+        </div>
       </div>
-      <div className='row'>
-        <CardForm />
-      </div>
-    </div>
-
-
-
-
-  )
+    )
+  }
 }
 
 export default App
