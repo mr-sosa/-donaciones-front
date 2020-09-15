3977f253b52f1528522a49abc46b37d44cb7aabd
mr-sosa <53924044+mr-sosa@users.noreply.github.com>
2020-09-15 18:56:50 -0500

    cosas varias

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import DonatorForm from './components/DonatorForm'
-import AmountForm from './components/AmountForm'
-import CardForm from './components/CardForm'
+import DonatorForm from './Components/DonatorForm'
+import AmountForm from './Components/AmountForm'
+import CardForm from './Components/CardForm'
 
 const ENDPOINT = 'http://localhost:2000'
 
