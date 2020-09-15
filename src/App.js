f5f12dbe17deddb847b59247fa4aa7e50b065925
mr-sosa <53924044+mr-sosa@users.noreply.github.com>
2020-09-15 09:56:31 -0500

    corrigiendo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import DonatorForm from './Components/DonatorForm'
-import AmountForm from './Components/AmountForm'
-import CardForm from './Components/CardForm'
+import DonatorForm from './components/DonatorForm'
+import AmountForm from './components/AmountForm'
+import CardForm from './components/CardForm'
 
 const ENDPOINT = 'http://localhost:2000'
 
